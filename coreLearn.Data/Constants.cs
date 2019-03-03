49e753d941a95508d72a9b6d4cdc0cd20e81b02e
Sanket Sonavane <2235668+sanketss84@users.noreply.github.com>
2019-03-03 19:47:31 +0530

    minor changes

--- coreLearn.Data/Constants.cs
+++ coreLearn.Data/Constants.cs
@@ -4,7 +4,7 @@ namespace coreLearn.Data
 {
     public static class Constants
     {
-        public const string MongoConnectionString = "mongodb://sanket:qawsed789@ds149501.mlab.com:49501";
+        public const string MongoConnectionString = "mongodb://username:password@ds149501.mlab.com:49501";
 
         public const string MongoDBName = "mongodb-hotproton";
 
